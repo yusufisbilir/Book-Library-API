6ef96627c50e3a7a35f23235e1da72158ce97f6d
yusufisbilir <dev.yusufisbilir@gmail.com>
2023-01-30 22:59:42 +0300

    Book model

---
+++ src/models/Book.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IBook {
+    title: string;
+    author: string;
+}
+
+export interface IBookModel extends IBook, Document {}
+
+const BookSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        author: { type: Schema.Types.ObjectId, ref: 'Author', required: true }
+    },
+    { timestamps: true, versionKey: true }
+);
+
+export default mongoose.model<IBookModel>('Book', BookSchema);
