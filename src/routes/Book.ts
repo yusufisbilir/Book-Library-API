d8dc2f5e7fab2dc3a3f6b16a57efdbd75c192977
yusufisbilir <dev.yusufisbilir@gmail.com>
2023-02-01 01:02:09 +0300

    Joi validation

--- src/routes/Book.ts
+++ src/routes/Book.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controllers/Book';
+import { validateSchema, Schemas } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', controller.createBook);
+router.post('/create', validateSchema(Schemas.book.create), controller.createBook);
 router.get('/get', controller.readAllBook);
 router.get('/get/:bookId', controller.readBook);
-router.patch('/update/:bookId', controller.updateBook);
+router.patch('/update/:bookId', validateSchema(Schemas.book.update), controller.updateBook);
 router.delete('/delete/:bookId', controller.deleteBook);
 
 export = router;
